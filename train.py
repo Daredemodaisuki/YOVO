cd7d5db81394c5fb6dbee42fd05406b906b69d66
Daredemodaisuki <six6666666666666@yeah.net>
2025-07-07 10:42:55 +0000

    Train YOVO on Ganji

--- train.py
+++ train.py
@@ -7,12 +7,12 @@ import shutil
 
 
 # 训练
-def train_yolo_model(dataset_dir, name, epochs=50, imgsz=640, batch=8, remote=True):
+def train_yolo_model(dataset_dir, dataset_yaml, model_path, name, epochs=50, imgsz=640, batch=8, remote=True):
     # model = YOLO('yolov8n.pt')  # 加载预训练模型（yolov8n.pt是小模型，适合快速训练）
-    model = YOLO('yaml/yolov8n (nc=62) - 去小核 - C2fFaster.yaml')
+    model = YOLO(model_path)
     # 训练模型
     results = model.train(
-        data=os.path.join(dataset_dir, 'data_B.yaml'),
+        data=os.path.join(dataset_dir, dataset_yaml),
         epochs=epochs,
         imgsz=imgsz,
         batch=batch,
@@ -29,8 +29,10 @@ def main():
     root = os.path.abspath(os.path.dirname(__file__))
 
     train_yolo_model(dataset_dir=os.path.join(root, "dataset"),
-                     name='yolo_origin去小核C2fFasrer-B100',
-                     epochs=100,
+                     dataset_yaml="data_Pseudo_Ganji.yaml",
+                     model_path='yaml/yolov8n (nc=62) - 去小核 - C2fFaster.yaml',
+                     name='yolo_origin去小核C2fFasrer-PGanji80',  # 文件夹名称
+                     epochs=80,
                      imgsz=196,
                      batch=16)
 
